ec77ce961652ab179eb12c1394ca3e9c8adea224
Sjunne <sjunneson@hotmail.com>
2022-05-13 09:26:08 +0200

    Done

---
+++ DataAccess/Movies/FullMovieDa.cs
@@ -0,0 +1,37 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Movies
+    {
+        [Table("movies")]
+        public class FullMovieDa
+        {
+            [ExplicitKey]
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public int Year { get; set; }
+            public string Poster { get; set; }
+            public string Plot { get; set; }
+            public string Rated { get; set; }
+            public string Released { get; set; }
+            public string Runtime { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Writer { get; set; }
+            public string Actors { get; set; }
+            public string Language { get; set; }
+            public string Country { get; set; }
+            public string Awards { get; set; }
+            public string imdbRating { get; set; }
+            public string imdbVotes { get; set; }
+            public string imdbID { get; set; }
+            public string Type { get; set; }
+            public string Response { get; set; }
+
+            public List<string> ActorList { get; set; }
+            public List<string> DirectorList { get; set; }
+        }
+    }
+
