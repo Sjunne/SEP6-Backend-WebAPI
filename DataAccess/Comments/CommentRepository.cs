79f4b56b7e037d84b12856ff409112c236a91a36
Sjunne <sjunneson@hotmail.com>
2022-05-30 10:49:45 +0200

    Indsat Using connection

--- DataAccess/Comments/CommentRepository.cs
+++ DataAccess/Comments/CommentRepository.cs
@@ -7,52 +7,65 @@ using Dapper;
 using DataAccess.Actors;
 using Dapper.Contrib.Extensions;
 using DataAccess.Movies;
+using System.Data.SqlClient;
 
 namespace DataAccess.Comments
 {
     public class CommentRepository : ICommentRepository
     {
-        private readonly IDbConnection _connection;
+        private readonly string _connection;
 
-        public CommentRepository(IDbConnection connection)
+        public CommentRepository(string connection)
         {
             _connection = connection;
         }
 
         public void DeleteComment(string id)
         {
-            _connection.Execute(@"DELETE FROM comments WHERE Id = @Id", new { Id = id });
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                _connection2.Execute(@"DELETE FROM comments WHERE Id = @Id", new { Id = id });
+            }
         }
 
         Comment ICommentRepository.CreateComment(Comment comment)
         {
-            var body = comment.body;
-            var username = comment.username;
-            var parentId = comment.parentId;
-            var createdAt = comment.createdAt;
-            var movieId = comment.movieId;
-            const string query = @"INSERT INTO comments (body, username, parentId, createdAt, movieId) 
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                var body = comment.body;
+                var username = comment.username;
+                var parentId = comment.parentId;
+                var createdAt = comment.createdAt;
+                var movieId = comment.movieId;
+                const string query = @"INSERT INTO comments (body, username, parentId, createdAt, movieId) 
             OUTPUT INSERTED.*
             VALUES (@body, @username, @parentId, @createdAt, @movieId)";
-            var output = _connection.QuerySingle<Comment>(query, new { body, username, parentId, createdAt, movieId });
-            return output;
+                var output = _connection2.QuerySingle<Comment>(query, new { body, username, parentId, createdAt, movieId });
+                return output;
+            }
         }
 
         List<Comment> ICommentRepository.GetComments(string id)
         {
-            
-            const string query = @"SELECT * FROM comments WHERE movieId = @id";
-            return (List<Comment>)_connection.Query<Comment>(query, new { id });
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                const string query = @"SELECT * FROM comments WHERE movieId = @id";
+                return (List<Comment>)_connection2.Query<Comment>(query, new { id });
+            }
         }
 
         Comment ICommentRepository.UpdateComment(Comment comment)
         {
-            var body = comment.body;
-            var id = comment.id;
-            const string query = @"UPDATE comments SET body = @body WHERE id = @id";
-          
-            _connection.Query<Comment>(query, new { body, id});
-            return _connection.Get<Comment>(id);
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+
+                var body = comment.body;
+                var id = comment.id;
+                const string query = @"UPDATE comments SET body = @body WHERE id = @id";
+
+                _connection2.Query<Comment>(query, new { body, id });
+                return _connection2.Get<Comment>(id);
+            }
         }
     }
 }
--- DataAccess/Movies/MovieRepository.cs
+++ DataAccess/Movies/MovieRepository.cs
@@ -7,55 +7,71 @@ using System.Linq;
 using Microsoft.VisualBasic;
 using DataAccess.Comments;
 using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
 
 namespace DataAccess.Movies
 {
     public class MovieRepository : IMovieRepository
     {
-        private readonly IDbConnection _connection;
+        private readonly string _connection;
 
-        public MovieRepository(IDbConnection connection)
+        public MovieRepository(string connection)
         {
             _connection = connection;
         }
 
         public MovieDa GetMovieByTitle(string title)
         {
-            const string query = @"SELECT * FROM movies WHERE title = @title";
-            return _connection.Query<MovieDa>(query, new {title}).FirstOrDefault();
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                const string query = @"SELECT * FROM movies WHERE title = @title";
+                return _connection2.Query<MovieDa>(query, new { title }).FirstOrDefault();
+            }
         }
 
         public List<MovieDa> GetMoviesAndImagesByTitle(string title)
         {
-            const string query = @"SELECT TOP(5) * FROM movies WHERE title LIKE @title + '%' 
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                const string query = @"SELECT TOP(5) * FROM movies WHERE title LIKE @title + '%' 
                                     ORDER BY CASE WHEN title = @title THEN 1 ELSE 2 END";
-            return (List<MovieDa>)_connection.Query<MovieDa>(query, new { title });
+                return (List<MovieDa>)_connection2.Query<MovieDa>(query, new { title });
+            }
         }
 
         public FavoriteMovieModel SetFavorite(FavoriteMovieModel model)
         {
-            var username = model.username;
-            var movieId = model.movieId;
-            var myBool = model.favorite;
-            const string query = @"IF EXISTS(select * from favorites where username = @username AND movieId = @movieId)
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                var username = model.username;
+                var movieId = model.movieId;
+                var myBool = model.favorite;
+                const string query = @"IF EXISTS(select * from favorites where username = @username AND movieId = @movieId)
                                  update favorites set favorite = @myBool where username = @username AND movieId = @movieId
                                  ELSE
                                  insert into favorites(username, movieId, favorite) values(@username, @movieId, @myBool);";
-           
-            return (FavoriteMovieModel)_connection.QueryFirstOrDefault<FavoriteMovieModel>(query, new { username, movieId, myBool });
+
+                return (FavoriteMovieModel)_connection2.QueryFirstOrDefault<FavoriteMovieModel>(query, new { username, movieId, myBool });
+            }
         }
 
         FavoriteMovieModel IMovieRepository.CheckFavorite(string email, string id)
         {
-            const string query = @"SELECT * FROM favorites WHERE username = @email AND movieId = @id";
-            return (FavoriteMovieModel)_connection.QueryFirstOrDefault<FavoriteMovieModel>(query, new { email, id });
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                const string query = @"SELECT * FROM favorites WHERE username = @email AND movieId = @id";
+                return (FavoriteMovieModel)_connection2.QueryFirstOrDefault<FavoriteMovieModel>(query, new { email, id });
+            }
         }
 
         List<FavoriteMovieModel> IMovieRepository.GetAllFavorites(string userProfile)
         {
-            const string query = @"SELECT [username],[movieId], [favorite], [title] FROM favorites 
+            using (var _connection2 = new SqlConnection(_connection))
+            {
+                const string query = @"SELECT [username],[movieId], [favorite], [title] FROM favorites 
                                    INNER JOIN dbo.movies ON movieId = movies.id WHERE username = @userProfile";
-            return (List<FavoriteMovieModel>)_connection.Query<FavoriteMovieModel>(query, new { userProfile });
+                return (List<FavoriteMovieModel>)_connection2.Query<FavoriteMovieModel>(query, new { userProfile });
+            }
         }
     }
 }
